7fc32b44433148d08c20b5df75dc2e7a2ff252ab
Femi Adetoye <davefm95@gmail.com>
2023-03-12 05:21:01 -0700

    added

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 """This is the base model module"""
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel():
     """Thid thr base model class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """This initializes the instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    form = "%Y-%m-%dT%H:%M:%S.%f"
+                    self.__dict__[key] = datetime.strptime(value, form)
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """The str representation of the class"""
@@ -18,11 +28,12 @@ class BaseModel():
 
     def save(self):
         """uodates self.updated_at"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns dict containing all instance attributes"""
-        dct = self.__dict__
+        dct = {}
+        dct.update(self.__dict__)
         dct["__class__"] = self.__class__.__name__
         dct["created_at"] = self.created_at.isoformat()
         dct["updated_at"] = self.updated_at.isoformat()
