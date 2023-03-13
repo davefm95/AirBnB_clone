6f86ae87b9480879690ddf6b0761a60b7673bc1d
Femi Adetoye <davefm95@gmail.com>
2023-03-13 15:19:41 -0700

    added

--- console.py
+++ console.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """The console.py Script"""
 import cmd
-import sys
+from importlib import import_module
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,5 +21,45 @@ class HBNBCommand(cmd.Cmd):
         """Handles eof character"""
         print()
         return True
+
+    def do_create(self, arg):
+        """Creates an instance"""
+        if not arg:
+            print("** class name missing **")
+            return
+        else:
+            clsmod = {'BaseModel': "models.base_model"}
+            for k, v in clsmod.items():
+                if k == arg:
+                    mymod = import_module(v)
+                    mycls = getattr(mymod, arg)
+                    obj = mycls()
+                    obj.save()
+                    print(f"{obj.id}")
+                    return
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """prints string rep of an instance"""
+        args = arg.split()
+        objs = storage.all()
+        if not args:
+            print("** class name missing **")
+            return
+        isvalidclass = 0
+        for key in objs.keys():
+            if args[0] == key.split(".")[0]:
+                isvalidclass = 1
+                if len(args) == 1:
+                    print("** instance id missing**")
+                    return
+                if args[1] == key.split(".")[1]:
+                    print(objs[key])
+                    return
+        if isvalidclass == 1:
+            print("** no instance found **")
+        else:
+            print("** class doesn't exist **")
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
